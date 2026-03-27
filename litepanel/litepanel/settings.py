4fdeaf93f7a8749d4fbecc5eed32228e4b7d6c64
LKY <jodhpur.laxman@gmail.com>
2026-03-28 03:44:59 +0530

    fix host

--- litepanel/litepanel/settings.py
+++ litepanel/litepanel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 FERNET_KEY = os.environ['FERNET_KEY']
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # ---------------------------------------------------------------------------
 # Application definition
